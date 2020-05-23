abdd58f1c2496ba20b515eba67f0d1b7c954c30f
KateMarshakova <62849370+KateMarshakova@users.noreply.github.com>
2020-05-23 11:35:49 +0300

    ADD( component)
    add component "dialogs", made design and address switch buttons.

--- add-react/react-start/src/components/Dialogs/Dialogs.jsx
+++ add-react/react-start/src/components/Dialogs/Dialogs.jsx
@@ -1,11 +1,40 @@
 import React from 'react';
 import c from './Dialogs.module.css';
+import { NavLink } from 'react-router-dom';
 
 
-const Dialogs = (props) =>{
+const Dialogs = (props) => {
     return (
         <div className={c.dialogs}>
-            Dialogs
+            <div className={`${c.control} ${c.bg}`}>
+                <button className={c.choiseBtn}>Messages</button>
+                <form>
+                    <input className={c.search} type='text' value='Search...'></input>
+                </form>
+            </div>
+            <div className={c.allDialogs}>
+                <div className={c.item}>
+                    <NavLink activeClassName={c.active} to='/dialogs/1'><button className={c.itemBtn}>Dima</button></NavLink>
+                </div>
+                <div className={c.item}>
+                    <NavLink activeClassName={c.active} to='/dialogs/2'><button className={c.itemBtn}>Arina</button></NavLink>
+                </div>
+                <div className={c.item}>
+                    <NavLink activeClassName={c.active} to='/dialogs/3'><button className={c.itemBtn}>Svetlana</button></NavLink>
+                </div>
+                <div className={c.item}>
+                    <NavLink activeClassName={c.active} to='/dialogs/4'><button className={c.itemBtn}>Maxim</button></NavLink>
+                </div>
+                <div className={c.item}>
+                    <NavLink activeClassName={c.active} to='/dialogs/5'><button className={c.itemBtn}>Sergey</button></NavLink>
+                </div>
+            </div>
+            <div className={`${c.write} ${c.bg} ${c.active}`}>
+                <div className={c.message}>Hi! How are you?</div>
+                <div className={c.message}>Hello! Not bad:) You?</div>
+                <div className={c.message}>Ok) I have interesting offer for you..</div>
+                <div className={c.message}>Which offer?</div>
+            </div>
         </div>
     );
 }
--- add-react/react-start/src/components/Profile/Profile.jsx
+++ add-react/react-start/src/components/Profile/Profile.jsx
@@ -5,19 +5,19 @@ import MyPosts from './MyPosts/MyPosts';
 const Profile = () => {
   return (
     <div className={c.profile}>
-      <div className={c['profile-cover']}>
+      <div className={`${c.profileCover} ${c.bg}`}>
         <div className={c.cover}>
           <h1 className={c.coverPhrase}>Adventure is life</h1>
           <img className={c.coverImage} src='https://thesologirlstravelguide.com/wp-content/uploads/2019/06/BeachBabeStock-0100-1-1024x683.jpg' alt='Cover' />
         </div>
       </div>
-      <div className={c['profile-avatar']}>
+      <div className={`${c.profileAvatar} ${c.bg}`}>
         <img className={c.avatar} src='https://i.pinimg.com/736x/89/66/5d/89665d7648d102d1df328a1a51dc11e1--summer-beach-summer-vibes.jpg' alt='Avatar' />
       </div>
-      <div className={c['profile-discription']}>
+      <div className={`${c.profiledDiscription} ${c.bg}`}>
         <div className={c.discription}>
           <div className={c.discriptionName}>
-            Maria Tomskaya
+            Tanya Tomskaya
             </div>
           <div className={c.discriptionDate}>
             <span className={c.label}>Birthday:</span>
